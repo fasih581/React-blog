e4c90d04431790382ddcc25ae37097c34a1efb18
fasih_ek <fasihedakkudi@gmail.com>
2024-03-13 17:01:47 +0530

    router commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-Blog-Project/',
+  base: '/React-blog/',
   plugins: [react()],
 })
