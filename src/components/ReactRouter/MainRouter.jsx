44de6f1c943fca5afe2cf7c18e8877e2d0654063
fasih_ek <fasihedakkudi@gmail.com>
2024-03-13 17:20:55 +0530

    router commit

--- src/components/ReactRouter/MainRouter.jsx
+++ src/components/ReactRouter/MainRouter.jsx
@@ -8,8 +8,8 @@ const MainRouter = () => {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/React-blog" element={<Home />} />
-        <Route path="admin" element={<AdminPanel />} />
+        <Route path="/react-blog-project" element={<Home />} />
+        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </>
   );
